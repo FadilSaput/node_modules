d21f9116cf7d33d643830aa255dce32b05d0d619
Fadli Saputra <152702279+FadilSaput@users.noreply.github.com>
2023-12-23 08:15:28 +0700

    main

---
+++ .vite/deps/@mui_material_Tabs.js
@@ -0,0 +1,27 @@
+"use client";
+import {
+  Tabs_default,
+  getTabsUtilityClass,
+  tabsClasses_default
+} from "./chunk-JWDY2MRL.js";
+import "./chunk-CCSKXIRI.js";
+import "./chunk-46AQ55S4.js";
+import "./chunk-LQYYIMB7.js";
+import "./chunk-DH3BJ3EP.js";
+import "./chunk-ZVPUTME5.js";
+import "./chunk-OMTM5ZK6.js";
+import "./chunk-PXEN5RK6.js";
+import "./chunk-2IQEPFHR.js";
+import "./chunk-ZFW33RP4.js";
+import "./chunk-GSZ7ISAW.js";
+import "./chunk-RZPB3X42.js";
+import "./chunk-EP2I3TOP.js";
+import "./chunk-Z6PCEVQI.js";
+import "./chunk-ZGRSIX2Q.js";
+import "./chunk-ROME4SDB.js";
+export {
+  Tabs_default as default,
+  getTabsUtilityClass,
+  tabsClasses_default as tabsClasses
+};
+//# sourceMappingURL=@mui_material_Tabs.js.map
